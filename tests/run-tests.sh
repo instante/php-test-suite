465c63ded8923be511eed06dd33a8d45b544bac5
Richard Ejem <richard@ejem.cz>
2016-06-15 13:59:48 +0200

    modified php.ini loading by run-tests script

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 pushd "$(dirname "$0")/.."
 
-if [ -f ./tests/php-unix.ini ]; then
+if [ -f "./tests/php-local.ini" ]; then
+    cp ./tests/php-local.ini ./tests/php.ini
+elif [ -f ./tests/php-unix.ini ]; then
     cp ./tests/php-unix.ini ./tests/php.ini
     echo "" >> ./tests/php.ini # empty line
-else
-    echo "" > ./tests/php.ini
+    PHP_EXT=`php -r "echo ini_get('extension_dir');"`
+    echo "extension_dir=$PHP_EXT" >> ./tests/php.ini
+elif [ -f "./tests/php.ini" ]; then # remove old php.ini from tests
+    rm "./tests/php.ini"
 fi
-PHP_EXT=`php -r "echo ini_get('extension_dir');"`
-echo "extension_dir=$PHP_EXT" >> ./tests/php.ini
 
 ./vendor/bin/tester ./tests/$1 -p php -c ./tests
 
