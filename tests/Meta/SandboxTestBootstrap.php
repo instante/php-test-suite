d7dda8a17beb63e3e338e2421f211ae961e51d75
Richard Ejem <richard@ejem.cz>
2016-06-16 17:35:55 +0200

    prevented loading CliPresenter

--- tests/Meta/SandboxTestBootstrap.php
+++ tests/Meta/SandboxTestBootstrap.php
@@ -3,8 +3,6 @@
 namespace Instante\Tests\Meta;
 
 use Instante\Tests\TestBootstrap;
-use Kdyby\Console\DI\ConsoleExtension;
-use Nette\Configurator;
 use Nette\Loaders\RobotLoader;
 
 class SandboxTestBootstrap extends TestBootstrap
@@ -14,15 +12,5 @@ class SandboxTestBootstrap extends TestBootstrap
         // appDir not needed there because sandbox app is located under tests dir
         // we need to avoid adding appDir to prevent duplicate indexing by RobotLoader
         $loader->addDirectory(static::$testsDir);
-    }
-
-    protected static function configureConfigurator(Configurator $configurator, $configDir)
-    {
-        parent::configureConfigurator($configurator, $configDir);
-        if (class_exists(ConsoleExtension::class)) {
-            // register console extension only when loaded,
-            // needed for Nette <=2.3
-            ConsoleExtension::register($configurator);
-        }
     }
 }
