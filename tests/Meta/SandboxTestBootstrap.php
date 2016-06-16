9f32a5cfa0045f2acdf022d4a9ff43638ab30d7f
Richard Ejem <richard@ejem.cz>
2016-06-16 18:03:34 +0200

    fixed cli presenter... again, hopefully finally.

--- tests/Meta/SandboxTestBootstrap.php
+++ tests/Meta/SandboxTestBootstrap.php
@@ -3,6 +3,8 @@
 namespace Instante\Tests\Meta;
 
 use Instante\Tests\TestBootstrap;
+use KdybyModule\CliPresenter;
+use Nette\Configurator;
 use Nette\Loaders\RobotLoader;
 
 class SandboxTestBootstrap extends TestBootstrap
@@ -12,5 +14,14 @@ class SandboxTestBootstrap extends TestBootstrap
         // appDir not needed there because sandbox app is located under tests dir
         // we need to avoid adding appDir to prevent duplicate indexing by RobotLoader
         $loader->addDirectory(static::$testsDir);
+    }
+
+    protected static function configureConfigurator(Configurator $configurator, $configDir)
+    {
+        parent::configureConfigurator($configurator, $configDir);
+        if (class_exists(CliPresenter::class)) {
+            // avoid loading CliPresenter service without required Kdyby\Console\Application
+            $configurator->addParameters(['application' => ['scanComposer' => FALSE]]);
+        }
     }
 }
