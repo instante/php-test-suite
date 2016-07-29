2ad289d5ccd3a5242bb9859a6410e42d68504360
Richard Ejem <richard@ejem.cz>
2016-07-29 19:11:30 +0200

    added spy() shortcut

--- src/shortcuts.php
+++ src/shortcuts.php
@@ -11,3 +11,15 @@ function mock()
 
     return call_user_func_array([\Mockery::class, 'mock'], $args);
 }
+
+/**
+ * shortcut to \Mockery::spy() - creating a spy/stub version of mock (ignores method calls with undefined expectations)
+ *
+ * @return \Mockery\MockInterface
+ */
+function spy()
+{
+    $args = func_get_args();
+
+    return call_user_func_array([\Mockery::class, 'spy'], $args);
+}
